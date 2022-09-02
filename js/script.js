8b5a0b7a8cab625db3d1607aff9e74cd251d59d1
Nabinchowdhury <nabinchowdhury26@gmail.com>
2022-09-02 23:31:08 +0600

    id generated

--- js/script.js
+++ js/script.js
@@ -7,17 +7,34 @@ const loadNewsItems = () => {
 }
 const showNewsItems = (datas) => {
     const newsItems = document.getElementById("news-items")
-
     for (const data of datas) {
-        console.log(data.category_name)
+        // console.log(data.category_name)
         const div = document.createElement('div')
-        div.classList.add("d-inline-flex", "pe-5")
+        div.classList.add("d-inline-flex", "px-4")
         div.innerHTML = `
         
-        <h5>${data.category_name} </h5>
+        <h6 onclick="loadNews('${data.category_id}')">${data.category_name} </h6>
         `
         newsItems.appendChild(div)
     }
 }
 
+const loadNews = (id) => {
+
+    const url = `https://openapi.programming-hero.com/api/news/category/${id}`
+    // console.log(url)
+    fetch(url)
+        .then(res => res.json())
+        .then(datas => console.log(datas))
+}
+
 loadNewsItems();
+
+
+
+
+
+
+
+// const sortedResponse = datas.sort(function (a, b) { return parseInt(b.category_id) - parseInt(a.category_id) });
+// console.log(sortedResponse)
