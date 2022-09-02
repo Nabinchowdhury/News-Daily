b779fc89d64816c85062eefa46c9bd838f941e5a
Nabinchowdhury <nabinchowdhury26@gmail.com>
2022-09-03 02:29:01 +0600

    dyamic card created

--- js/script.js
+++ js/script.js
@@ -38,6 +38,7 @@ const showNews = (datas, name) => {
 
     const foundMsg = document.getElementById('found-msg')
     const newsCard = document.getElementById("news-card")
+    newsCard.innerHTML = ``
 
 
     if (datas && name) {
@@ -52,28 +53,60 @@ const showNews = (datas, name) => {
         newsCard.innerHTML = ""
     }
 
-    // for (const each of sortedResponse) {
-    //     console.log(each)
-    //     const news = document.createElement('div')
-    //     news.classList.add('card', 'mb-3', "border-0")
-    //     news.innerHTML = `
-    //     <div class="row g-0 ">
-    //     <div class="col-md-4">
-    //                     <img src="${each.thumbnail_url}" class="img-fluid rounded-start w-75 m-3" style="height: 300px;" "alt="...">
-    //                 </div>
-    //                 <div class="col-md-8">
-    //                     <div class="card-body">
-    //                         <h5 class="card-title">Card title</h5>
-    //                         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
-    //                             additional content. This content is a little bit longer.</p>
-    //                         <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    //                     </div>
-    //                 </div>
-    //                 </div>
-    //     `
-    //     newsCard.appendChild(news)
+    for (const each of sortedResponse) {
+        // console.log(each)
 
-    // }
+        const { author, details, thumbnail_url, title, total_view } = each
+        // console.log(author)
+        const { name, published_date, img } = author
+
+        const news = document.createElement('div')
+        news.classList.add('card', 'mb-3', "border-0")
+        news.innerHTML = `
+        <div class="row g-0 ">
+        <div class="col-md-3">
+                        <img src="${thumbnail_url}" class="img-fluid rounded-start w-100 m-3" style="height: 300px;" "alt="...">
+                    </div>
+                    <div class="col-md-9">
+                        <div class="card-body ms-5" >
+                            <h5 class="card-title">${title}</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This content is a little bit longer.</p>
+                               <div class="d-flex">
+                            <div class="d-flex align-items-center  w-25">
+                            <img src="${img}" class="rounded-circle"
+                                style="height: 50px; width:50px;" alt="">
+                                <div class="ms-2">
+                                <p class="mb-0 mt-2">${name}</p>
+                            <p class="ms-2">${published_date ? published_date.slice(0, 10) : "No Date Found"}</p>
+                                </div>
+                            
+                        </div>
+
+                        <div class="d-flex align-items-center justify-content-center w-25">
+                            <i class="fa-solid fa-eye"></i>
+                            
+                            <p class="ms-2 mt-2" id="views">${total_view ? total_view : "No Views"}</p>
+                        </div>
+                        <div class="d-flex align-items-center justify-content-center w-25">
+                            <i class="fa-regular fa-star-half-stroke"></i>
+                            <i class="fa-regular fa-star"></i>
+                            <i class="fa-regular fa-star"></i>
+                            <i class="fa-regular fa-star"></i>
+                            <i class="fa-regular fa-star"></i>
+                        </div>
+                        <div class="d-flex align-items-center justify-content-center w-25">
+                            <i class="fa-solid fa-arrow-right"></i>
+                        </div>
+                    </div>
+                        </div>
+                    </div>
+                    </div>
+        `
+        newsCard.appendChild(news)
+
+
+    }
 
 
 }
