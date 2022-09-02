17112cff0a5f5d392cb76275f1fef7f94bf467e7
Nabinchowdhury <nabinchowdhury26@gmail.com>
2022-09-03 01:54:34 +0600

    card layout created

--- js/script.js
+++ js/script.js
@@ -30,13 +30,16 @@ const loadNews = (id, name) => {
 
 }
 const showNews = (datas, name) => {
-    // console.log(datas)
-    // console.log(name)
+
     const sortedResponse = datas.sort(function (a, b) {
         return parseInt(b.total_view) - parseInt(a.total_view)
     });
     console.log(sortedResponse)
+
     const foundMsg = document.getElementById('found-msg')
+    const newsCard = document.getElementById("news-card")
+
+
     if (datas && name) {
 
         foundMsg.classList.remove("d-none")
@@ -46,7 +49,31 @@ const showNews = (datas, name) => {
     }
     else {
         foundMsg.classList.add("d-none")
+        newsCard.innerHTML = ""
     }
+
+    // for (const each of sortedResponse) {
+    //     console.log(each)
+    //     const news = document.createElement('div')
+    //     news.classList.add('card', 'mb-3', "border-0")
+    //     news.innerHTML = `
+    //     <div class="row g-0 ">
+    //     <div class="col-md-4">
+    //                     <img src="${each.thumbnail_url}" class="img-fluid rounded-start w-75 m-3" style="height: 300px;" "alt="...">
+    //                 </div>
+    //                 <div class="col-md-8">
+    //                     <div class="card-body">
+    //                         <h5 class="card-title">Card title</h5>
+    //                         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+    //                             additional content. This content is a little bit longer.</p>
+    //                         <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    //                     </div>
+    //                 </div>
+    //                 </div>
+    //     `
+    //     newsCard.appendChild(news)
+
+    // }
 
 
 }
